93626820096b7bc9b750f57c43ef4edc5e3dacb8
Belingham <kuremafijas5@gmail.com>
2025-01-07 19:30:09 +0100

    Add ingredient

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -79,3 +79,13 @@ class RecipeModelTests(TestCase):
         )
         self.assertEqual(str(tag), tag.name)
         self.assertEqual(tag.user, user)
+
+    def test_create_ingredient(self):
+        """Test creating a new ingredient successful"""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -61,4 +61,20 @@ class TagViewSet(mixins.UpdateModelMixin,
 
     def perform_create(self, serializer):
         """Create a new tag"""
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Retrieve the ingredients for the authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
         serializer.save(user=self.request.user)
